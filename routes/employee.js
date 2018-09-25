9379a88d2fbae6952ce68cf78052412f9675ac79
tejaswi-garimella27 <43320402+tejaswi-garimella27@users.noreply.github.com>
2018-09-25 09:48:47 +0530

    upadated pay-period/rounding 50 cents

--- routes/employee.js
+++ routes/employee.js
@@ -17,53 +17,39 @@ function getIncomeTax(){
      incomeTax = 0;
   }
   else if(income <= 37000){
-    cCount = (income-18200)*19;
-    incomeTax = (cCount/50);
+    income = income - 18200;
+    cCount = (income*0.19);
+    incomeTax = Math.round(cCount/12);
   }
   else if(income <= 87000){
-    // cCount = (37000-18200)*19;
-    // incomeTax = (cCount/50);
-    // cCount = cCount%50;
-    cCount = (income-37000)*32.5;
-    incomeTax = (cCount/50);
+    income = income - 37000;
+    cCount = (income*0.325) + 3572;
+    incomeTax = Math.round(cCount/12);
   }
   else if(income <= 180000){
-    // cCount = (37000-18200)*19;
-    // incomeTax = (cCount/50);
-    // cCount = cCount%50;
-    // cCount += (87000-37000)*32.5;
-    // incomeTax += (cCount/50);
-    // cCount = cCount%50;
-    cCount = (income-87000)*37;
-    incomeTax = (cCount/50);
+    income = income - 87000;
+    cCount = (income*0.37) + 19822;
+    incomeTax = Math.round(cCount/12);
   }
   else{
-    // cCount = (37000-18200)*19;
-    // incomeTax = (cCount/50);
-    // cCount = cCount%50;
-    // cCount += (87000-37000)*32.5;
-    // incomeTax += (cCount/50);
-    // cCount = cCount%50;
-    // cCount += (180000-87000)*37;
-    // incomeTax += (cCount/50);
-    // cCount = cCount%50;
-    cCount = (income-180000)*45;
-    incomeTax = (cCount/50);
+    income = income - 180000;
+    cCount = (income*0.45) + 54232;
+    incomeTax = Math.round(cCount/12);
   }
   return incomeTax;
 }
 
 function getCalenderMonth(){
 
-  const monthNames = ["january", "february", "march", "april", "may", "june",
-  "july", "august", "september", "october", "november", "december","january"
-];
+//   const monthNames = ["january", "february", "march", "april", "may", "june",
+//   "july", "august", "september", "october", "november", "december","january"
+// ];
 
-  let date = new Date(this['payment-start-date']);
-  let date1 = new Date('01-'+monthNames[date.getMonth()+1]+'-'+date.getFullYear());
-  return this['payment-start-date'].substr(0,2) + ' ' + monthNames[date.getMonth()] + ' , ' +date.getFullYear()+ '  -  ' +
-         date1.getUTCDate() + ' ' + monthNames[date.getMonth()] + ' , ' +date.getFullYear();
-
+//   let date = new Date(this['payment-start-date']);
+//   let date1 = new Date('01-'+monthNames[date.getMonth()+1]+'-'+date.getFullYear());
+//   return this['payment-start-date'].substr(0,2) + ' ' + monthNames[date.getMonth()] + ' , ' +date.getFullYear()+ '  -  ' +
+//          date1.getUTCDate() + ' ' + monthNames[date.getMonth()] + ' , ' +date.getFullYear();
+      return this['payment-start-date'];
 }
 
 router.post('/', function(req, res, next) {
@@ -71,10 +57,10 @@ router.post('/', function(req, res, next) {
   
   response['name'] = getName.call(req.body);
   response['pay-period'] = getCalenderMonth.call(req.body);
-  response['gross-income'] = getGrossIncome.call(req.body);
-  response['income-tax'] = getIncomeTax.call(req.body);
-  response['net-income'] = response['gross-income'] - response['income-tax'];
-  response['super-amount'] = response['gross-income'] * req.body['super-rate']/100;
+  response['gross-income'] = parseInt(getGrossIncome.call(req.body));
+  response['income-tax'] = parseInt(getIncomeTax.call(req.body));
+  response['net-income'] = parseInt(response['gross-income'] - response['income-tax']);
+  response['super-amount'] = parseInt(Math.round(response['gross-income'] * req.body['super-rate']/100));
   res.send(response);
 });
 
